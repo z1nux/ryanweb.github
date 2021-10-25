25fd3125ee637428842f0cea6f74f5e1cb869acc
z1nux <daf.ryandhana@gmail.com>
2021-10-25 14:59:10 +0700

    semester 3 kek anjing

---
+++ coding1.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="height=device-height, width=device-width, initial-scale=1.0">
+        <title>Damfa</title>
+        
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+
+            main{
+                padding: 20px;
+                min-width: fit-content;
+            }
+            
+
+            body{
+                font-family: 'Bebas Neue', cursive;
+                background-color: #303036;
+                margin: 0;
+                padding: 0;           
+            }
+
+            .judul{
+                text-align: center;
+                color: #ff6a00;
+                font-size: 40px;
+                text-shadow: 2px 1px 0px #ff6a00;
+                text-rendering: geometricPrecision;
+                
+            }
+
+            .exp{
+                text-align: center;
+                color: whitesmoke;
+                
+            }
+
+            .SD{
+                text-align: center;
+                text-shadow: transparent;
+                color: #ff6a00;
+                margin-top: 50px;
+            }
+            
+            .bio{
+                margin-top: 50px;
+                line-height: 1mm;
+                text-align: center;
+            }
+            .logo-ig{
+                width: 100px;
+                margin-right: 50px;
+            }
+
+            .logo-gh{
+                width: 100px;
+            }
+
+            .kaki{
+                text-align: center;
+                background-color: #ff6a00;
+                color: whitesmoke;
+                padding: 20px;
+                margin-top: 50px;
+            }
+        </style>
+        
+    </head>
+
+    <body>
+        <header>
+            <div class="judul">
+                <h1> Welcome to dapa web</h1>
+            </div>
+
+            <div class="exp"> 
+                <tr>
+                <td>Dapa</td>
+                <td>|</td>
+                <td>Web Dapa</td>
+                <td></td>
+                </tr>
+            </div>
+
+        </header>
+
+        <main>
+            
+            <div>
+                <article class="bio">
+                    <h3 class="SD">A little about me</h3>
+                    <div class="exp">
+                        <p>I'm Daffa I'm student of Singaperbangsa Karawang University</p>
+                        <p>jangan lupa tugas GCRnya ya gaes</p>
+                        <p>asli ribet banget kan</p>
+                    </div>    
+                </article>
+            </div>
+            
+            <div class="SD">
+                <article>
+                    <h3>Social Media</h3>
+                <tr>
+                    <td><a href="https://instagram.com/ryandhanaaa" target="blank" rel="norefferer"><img src="https://www.freepnglogos.com/uploads/instagram-logos-png-images-free-download-2.png" alt="Instagram Logo" class="logo-ig"></a></td>
+                    <td><a href="https://github.com/z1nux" target="blank" rel="norefferer"><img src="https://image.flaticon.com/icons/png/512/25/25231.png" alt="Github Logo" class="logo-gh"></a></td>
+                </tr>
+
+                <p>You can click the link if you want to collab!</p>
+                </article>
+                
+            </div>
+
+        </main>
+
+        <footer class="kaki">
+              <p>its empty yow, calm down i still learning</p>
+        </footer>
+    </body>
+</html>
